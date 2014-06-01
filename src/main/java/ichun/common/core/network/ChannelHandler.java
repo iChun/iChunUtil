c332fec7bf0f5ff66ece0678d133098ceca72e4c
iChun <ohai.iChun@gmail.com>
2014-06-02 03:41:34 +0800

    Add @Sharable so netty won't complain.

--- src/main/java/ichun/common/core/network/ChannelHandler.java
+++ src/main/java/ichun/common/core/network/ChannelHandler.java
@@ -57,6 +57,7 @@ public class ChannelHandler extends FMLIndexedMessageToMessageCodec<AbstractPack
         return handlers;
     }
 
+    @Sharable
     private static class PacketExecuter extends SimpleChannelInboundHandler<AbstractPacket>
     {
         @Override
