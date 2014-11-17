a8ff1c942fb4d5ada7ee0ba690be5ab5c7bdf378
iChun <ohai.iChun@gmail.com>
2014-11-17 00:04:21 +0000

    More stuff with Tabula

--- src/main/java/ichun/common/core/util/MD5Checksum.java
+++ src/main/java/ichun/common/core/util/MD5Checksum.java
@@ -13,6 +13,7 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.text.DecimalFormat;
 
 public class MD5Checksum {
 
@@ -67,4 +68,11 @@ public class MD5Checksum {
 		}
 		return new String(hex, "ASCII");
 			}
+
+    public static String readableFileSize(long size) {
+        if(size <= 0) return "0";
+        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
 }
