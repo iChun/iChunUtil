6072a2aea06e5fd26efb699a25fa802cad4ae1a7
iChun <ohai.iChun@gmail.com>
2017-03-22 12:49:53 +0800

    Fix crashing with Waila/Wawla.

--- src/main/java/me/ichun/mods/ichunutil/common/entity/EntityBlock.java
+++ src/main/java/me/ichun/mods/ichunutil/common/entity/EntityBlock.java
@@ -78,6 +78,8 @@ public class EntityBlock extends Entity
         preventEntitySpawning = true;
         isImmuneToFire = true;
         canDropItems = true;
+        blocks = new IBlockState[0][0][0];
+        tileEntityNBTs = new NBTTagCompound[0][0][0];
     }
 
     @Nullable
