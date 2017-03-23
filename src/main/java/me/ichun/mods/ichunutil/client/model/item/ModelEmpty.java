4d0fb57dc0822c024a5606e749d709970318f860
iChun <ohai.iChun@gmail.com>
2017-03-23 17:59:20 +0800

    Nothing to see here....

---
+++ src/main/java/me/ichun/mods/ichunutil/client/model/item/ModelEmpty.java
@@ -0,0 +1,66 @@
+package me.ichun.mods.ichunutil.client.model.item;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemOverrideList;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+
+@SuppressWarnings("deprecation")
+public class ModelEmpty implements IBakedModel
+{
+    public static ModelEmpty INSTANCE = new ModelEmpty();
+
+    private static final List<BakedQuad> DUMMY_LIST = Collections.emptyList();
+
+    private ModelEmpty(){}
+
+    @Override
+    public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand)
+    {
+        return DUMMY_LIST;
+    }
+
+    @Override
+    public boolean isAmbientOcclusion()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isGui3d()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isBuiltInRenderer()
+    {
+        return false;
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleTexture()
+    {
+        return Minecraft.getMinecraft().getTextureMapBlocks().getMissingSprite();
+    }
+
+    @Override
+    public ItemCameraTransforms getItemCameraTransforms()
+    {
+        return ItemCameraTransforms.DEFAULT;
+    }
+
+    @Override
+    public ItemOverrideList getOverrides()
+    {
+        return ItemOverrideList.NONE;
+    }
+}
