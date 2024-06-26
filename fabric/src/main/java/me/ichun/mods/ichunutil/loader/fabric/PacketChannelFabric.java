d6fbf4157752dba3a68260264436060a858ed374
iChun <ohai.iChun@gmail.com>
2024-06-26 01:35:17 +0100

    Remove import.

--- fabric/src/main/java/me/ichun/mods/ichunutil/loader/fabric/PacketChannelFabric.java
+++ fabric/src/main/java/me/ichun/mods/ichunutil/loader/fabric/PacketChannelFabric.java
@@ -14,7 +14,6 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
