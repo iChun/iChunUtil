948dd01b8b85f7a59030d4354ab6f2ce13742f24
iChun <ohai.iChun@gmail.com>
2025-05-02 22:22:48 +0100

    Actually fix Mod Menu Integration - iChunUtil delegates it all.

--- fabric/src/main/java/me/ichun/mods/ichunutil/loader/fabric/client/ModMenuIntegration.java
+++ fabric/src/main/java/me/ichun/mods/ichunutil/loader/fabric/client/ModMenuIntegration.java
@@ -4,12 +4,19 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.ichun.mods.ichunutil.client.gui.config.WorkspaceConfigs;
 import me.ichun.mods.ichunutil.common.config.ConfigBase;
+import me.ichun.mods.ichunutil.common.iChunUtil;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ModMenuIntegration implements ModMenuApi
 {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory()
+    {
+        return screen -> new WorkspaceConfigs(screen, iChunUtil.MOD_ID);
+    }
+
     @Override
     public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories()
     {
